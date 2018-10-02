cb2220eb1a6eec4597b2beab5efc5ee1b9168107
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-10-02 15:29:32 -0300

    Separate encode and decode methods

--- src/Ssess.php
+++ src/Ssess.php
@@ -25,35 +25,22 @@ class Ssess implements \SessionHandlerInterface
     public function read($session_id)
     {
         $file_name = $this->getFileName($session_id);
-        $encrypted_data = @file_get_contents("$this->savePath/$file_name");
 
-        if (!$encrypted_data) {
+        $content = @file_get_contents("$this->savePath/$file_name");
+
+        if (!$content) {
             return '';
         }
 
-        $encrypted_data = json_decode($encrypted_data);
-
-        if (!$encrypted_data) {
-            return '';
-        }
-
-        $iv = base64_decode($encrypted_data->iv);
-
-        return openssl_decrypt($encrypted_data->data, $this->cipher, $session_id, 0, $iv);
+        return $this->decode($session_id, $content);
     }
 
     public function write($session_id, $session_data)
     {
         $file_name = $this->getFileName($session_id);
 
-        $iv_length = openssl_cipher_iv_length($this->cipher);
-        $iv = openssl_random_pseudo_bytes($iv_length);
-        $encrypted_data = openssl_encrypt($session_data, $this->cipher, $session_id, 0, $iv);
+        $content = $this->encode($session_id, $session_data);
 
-        $content = json_encode([
-            'data' => $encrypted_data,
-            'iv' => base64_encode($iv)
-        ]);
         return file_put_contents("$this->savePath/$file_name", $content) !== false;
     }
 
@@ -78,6 +65,31 @@ class Ssess implements \SessionHandlerInterface
         }
 
         return true;
+    }
+
+    private function encode($session_id, $session_data)
+    {
+        $iv_length = openssl_cipher_iv_length($this->cipher);
+        $iv = openssl_random_pseudo_bytes($iv_length);
+        $encrypted_data = openssl_encrypt($session_data, $this->cipher, $session_id, 0, $iv);
+
+        return json_encode([
+            'data' => $encrypted_data,
+            'iv' => base64_encode($iv)
+        ]);
+    }
+
+    private function decode($session_id, $content)
+    {
+        $encrypted_data = json_decode($content);
+
+        if (!$encrypted_data) {
+            return '';
+        }
+
+        $iv = base64_decode($encrypted_data->iv);
+
+        return openssl_decrypt($encrypted_data->data, $this->cipher, $session_id, 0, $iv);
     }
 
     private function getFileName($session_id)
